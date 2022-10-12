edc30aeb40c3700faa1e5c3b3fff38588f4bbe1b
Pathways012 <64288063+Pathways012@users.noreply.github.com>
2022-10-12 07:49:31 -0700

    Update FindPrimesUpTo.py

--- FindPrimesUpTo.py
+++ FindPrimesUpTo.py
@@ -2,7 +2,7 @@
 limit = 1000
 
 def CheckIfPrimeNumber(number):
-    for i in range(2, int(number/2)):
+    for i in range(2, int(math.sqrt(number))+1):
         if(number%i == 0):
             return False
     print(number)
